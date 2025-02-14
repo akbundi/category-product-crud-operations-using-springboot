c43910622790d31c1afd44647932ed645be3dc85
akbundi <66117217+akbundi@users.noreply.github.com>
2025-02-14 20:18:57 +0530

    Add files via upload

---
+++ crud-operation-category-product/src/main/java/com/nimap/crud_operation_category_product/CrudOperationCategoryProductApplication.java
@@ -0,0 +1,13 @@
+package com.nimap.crud_operation_category_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationCategoryProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOperationCategoryProductApplication.class, args);
+	}
+
+}
---
+++ crud-operation-category-product/src/test/java/com/nimap/crud_operation_category_product/CrudOperationCategoryProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimap.crud_operation_category_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOperationCategoryProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
